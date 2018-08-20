677bd427335cd284f054cd8088f9e944239d00aa
KaiLight <lastgrunger@gmail.com>
2018-08-20 19:34:48 +0300

    Some bugfix

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.info('Hello world')
+
+
+
+const main = async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+
+  await Promise.resolve('ok');
+}
+
+
+
+(async () => {
+  console.log(await main());
+})()
